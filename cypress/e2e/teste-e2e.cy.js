de15d4f899fd0d56a765f3db74dde0b4d715b56c
Luany Rudic <luany-r@hotmail.com>
2025-10-16 23:18:13 -0300

    adicionando workflow

--- cypress/e2e/teste-e2e.cy.js
+++ cypress/e2e/teste-e2e.cy.js
@@ -1,40 +1,45 @@
 /// <reference types="cypress" />
 
-describe('Testes End to end - QA Playground', () => {
+describe("Testes End to end - QA Playground", () => {
+  beforeEach(() => {
+    cy.visit("playground-html");
+  });
 
-    beforeEach(() => {
-        cy.visit('https://qa-impact.github.io/playground-html')
-    });
-    
-    it('Deve preencher o formulário com sucesso', () => {
-        cy.preencherForm('Fábio', 'fabio@teste.com', 30)
-        cy.get('#form-feedback').should('contain', 'Formulário enviado com sucesso!')
-    });
+  afterEach(() => {
+    cy.screenshot();
+  });
 
-    it('Deve exibir mensagem de erro quando não preencher o campo nome', () => {
-        cy.preencherForm('', 'fabio@teste.com', 30)        
-        cy.get('#name').should('match', ':invalid')
-    });
+  it("Deve preencher o formulário com sucesso", () => {
+    cy.preencherForm("Luany", "luany@teste.com", 27);
+    cy.get("#form-feedback").should(
+      "contain",
+      "Formulário enviado com sucesso!"
+    );
+  });
 
-    it('Deve exibir mensagem de erro quando não preencher o campo e-mail', () => {
-        cy.preencherForm('Fábio', '', 30)        
-        cy.get('#email').should('match', ':invalid')
-    });
+  it("Deve exibir mensagem de erro quando não preencher o campo nome", () => {
+    cy.preencherForm("", "luany@teste.com", 27);
+    cy.get("#name").should("match", ":invalid");
+  });
 
-    it('Deve exibir mensagem de erro quando não preencher o campo Idade', () => {
-        cy.preencherForm('Fábio', 'fabio@teste.com', '')        
-        cy.get('#age').should('match', ':invalid')
-    });
+  it("Deve exibir mensagem de erro quando não preencher o campo e-mail", () => {
+    cy.preencherForm("Luany", "", 27);
+    cy.get("#email").should("match", ":invalid");
+  });
 
-    it('Deve exibir mensagem de erro quando preencher e-mail com formato inválido', () => {
-        cy.preencherForm('Fábio', 'fabio.com', 30)        
-        cy.get('#email').should('match', ':invalid')
-    });
+  it("Deve exibir mensagem de erro quando não preencher o campo Idade", () => {
+    cy.preencherForm("Luany", "luany@teste.com", "");
+    cy.get("#age").should("match", ":invalid");
+  });
 
-    it('Deve ativar e validar o modo de alto contraste', () => {
-        cy.get('body').should('not.have.class', 'high-contrast')
-        cy.get('#toggle-contrast').click()
-        cy.get('body').should('have.class', 'high-contrast')
-      })
+  it("Deve exibir mensagem de erro quando preencher e-mail com formato inválido", () => {
+    cy.preencherForm("Luany", "luany.com", 27);
+    cy.get("#email").should("match", ":invalid");
+  });
 
+  it("Deve ativar e validar o modo de alto contraste", () => {
+    cy.get("body").should("not.have.class", "high-contrast");
+    cy.get("#toggle-contrast").click();
+    cy.get("body").should("have.class", "high-contrast");
+  });
 });
